4981bde2fea7fd03cbd55de4c382b84441fbad04
Om Patel <omunite21@gmail.com>
2024-05-07 11:30:36 +0530

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import './ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${lusitana.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
